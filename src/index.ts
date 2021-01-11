a35a59a03775e9a23a125fb4404b68f344129ec1
jk <ecar1234@gmail.com>
2021-01-11 17:33:41 +0900

    example #4 class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-interface Human {
-    name: string,
-    gender: string,
-    age: number
+class Human {
+   public name: string
+   public age: number
+   public gender: string
+   constructor(name:string, age:number, gender:string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+   }
 }
+const lynn = new Human("Lynn", 18, "female")
 
 const person = {
     name: "JK",
@@ -14,7 +20,7 @@ const sayHi = (person:Human):string => {
     return `hello ${person.name}, you are ${person.age}, you are ${person.gender}`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 
 export {}; 
