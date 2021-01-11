fd281a49779a4b05a7323ab85015a5a7f2e4b6a8
jk <ecar1234@gmail.com>
2021-01-11 17:21:33 +0900

    example #3 interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-const name = "jk", age = 24, gender = "male";
-const sayHi = (name:string, age:number, gender:string):string => {
-    return `hello ${name}m you are ${age}, you are ${gender}`;
+interface Human {
+    name: string,
+    gender: string,
+    age: number
 }
 
-console.log(sayHi("js",74,"male"));
+const person = {
+    name: "JK",
+    gender: "male",
+    age: 25
+}
+const name = "jk", age = 24, gender = "male";
+const sayHi = (person:Human):string => {
+    return `hello ${person.name}, you are ${person.age}, you are ${person.gender}`;
+}
+
+console.log(sayHi(person));
 
 
 export {}; 
