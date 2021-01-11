6c1d50a0f91865b510930a80814788b4920c2867
jk <ecar1234@gmail.com>
2021-01-11 23:24:24 +0900

    example #5 create blockchain2(using static,privaate,crypto)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJs from 'crypto-js';
+
 class Block {
     public index:number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+    
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index:number,
         hash: string,
@@ -19,11 +29,19 @@ class Block {
     }
 }
 
+// Block.calculateBlockHash()
+
 const genesisBlock:Block = new Block(0, "202020", "", "hello", 123456);
 
 let blockchain: [Block] = [genesisBlock];
 
-console.log(blockchain)
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatsBlock = (): Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
 
 
 export {}
