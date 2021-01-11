41cb6a9f7c8579c705c70cb2c4a839fcac9412f0
jk <ecar1234@gmail.com>
2021-01-11 16:09:12 +0900

    example #1

---
+++ index.ts
@@ -0,0 +1,9 @@
+const name = "jk", age = 24, gender = "male";
+const sayHi = (name, age, gender?) => {
+    console.log(`hello ${name}m you are ${age}, you are ${gender}`);
+}
+
+sayHi(name,age)
+
+
+export {};
